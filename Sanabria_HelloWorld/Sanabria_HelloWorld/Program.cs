b4dae411ff18c64f0be1e9138b50165dd54e8863
Elizabeth Sanabria <eas2062@rit.edu>
2023-08-29 10:00:10 -0400

    pushing helloworld to git

---
+++ Sanabria_HelloWorld/Sanabria_HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Sanabria_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elizabeth Sanabria");
+            string wolfString = "wolf";
+            Console.WriteLine(wolfString + " is a " + wolfString);
+            Console.WriteLine("5 + 91 equals:" + (5 + 91));
+            if (wolfString == "wolf")
+            {
+                Console.WriteLine("oh boy a wolf");
+            } else
+            {
+                Console.WriteLine("not a wolf?? impossible");
+            }
+        }
+    }
+}
