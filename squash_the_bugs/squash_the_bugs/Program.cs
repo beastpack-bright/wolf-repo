103a10f6558f6593e5d883c5e1c63d5cc80403ba
Elizabeth Sanabria <eas2062@rit.edu>
2023-08-31 08:58:00 -0400

    bug squash squish!

---
+++ squash_the_bugs/squash_the_bugs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace squash_the_bugs
+{
+    // Class Program
+    // Author: David Schuh
+    // Purpose: Bug squashing exercise
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Loop through the numbers 1 through 10 
+        //          Output N/(N-1) for all 10 numbers
+        //          and list all numbers processed
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            // declare int counter
+            // int i = 0 = This is an error because it doesn't have the proper line ending of ;
+            int i = 0;
+            // declare string to hold all numbers
+            string allNumbers = null;
+            // loop through the numbers 1 through 10
+            for (i = 1; i < 10; ++i)
+            {
+
+                // output explanation of calculation
+                //  Console.Write(i + "/" + i - 1 + " = "); This is trying to add strings and integers together, which isn't possible. Fixed by separating the maths part from the rest using ()
+                Console.WriteLine(i + "/" + (i - 1) + "=");
+
+                // output the calculation based on the numbers
+                if (i == 1)
+                {
+                    Console.WriteLine("Impossible"); //This will fix the divide by zero error created by dividing 1 by 0 in the normal calculation, by checking if i is 1 and declaring its impossible.
+                }
+                else
+                {
+                    Console.WriteLine(i / (i - 1)); //The normal line.
+                }
+
+
+                // concatenate each number to allNumbers
+                allNumbers += i + " ";
+
+                // increment the counter
+                i = i + 1;
+            }
+
+            // output all numbers which have been processed
+            //Console.WriteLine("These numbers have been processed: " allNumbers); This line is not properly using + and , to properly tell the console to write both the string and the variable value.
+            //Furthermore since of where the brackets are placed, allNumbers is not a part of the current context, so the allNumbers line has been moved.
+            Console.WriteLine("These numbers have been processed:" + allNumbers);
+        }
+    }
+}
