c97f50b20288b942206d224c21d32b86251f3795
Elizabeth Sanabria <eas2062@rit.edu>
2023-09-17 21:57:43 -0400

    IT WORKS!!

--- MadLibs/MadLibs/Program.cs
+++ MadLibs/MadLibs/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MadLibs
 {
@@ -11,12 +12,40 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
+            bool playGame = false;
+            int answerCorrect = 1; //To mess with the player for being BAD.
+            while (playGame == false)
+            {
+                Console.WriteLine("Do you want to play Mad Libs?");
+                string answer = Console.ReadLine();
+                if (answer == "yes" ||  answer == "Yes" || answer == "y" || answer == "ye") {
+                    Console.WriteLine(" Great!");
+                        playGame = true; }
+                else if (answer == "no" || answer == "No" || answer == "n" || answer == "n o")
+                {
+                    Console.WriteLine("No? Okay... ");
+                    Environment.Exit(0);
+                } else
+                {
+                    if (answerCorrect >= 3)
+                    {
+                        Console.WriteLine("Please answer yes or no. The wild wolves are already preparing to come to your house.");
+                            Console.WriteLine("They eat computers by the way. Your Tetris high score, GONE!");
+                    } else
+                    {
+                        answerCorrect++;
+                                            Console.WriteLine("Please answer yes or no.");
+                    }
+                    
+                }
+            }
             //Setting up starting variables
             int numLibs = 0;
             int cntr = 0; 
             int nChoice = 0;
             string finalStory = "";
             string resultString = "";
+            
             //Setting up the StreamReader for the external text file
             StreamReader input;
 
