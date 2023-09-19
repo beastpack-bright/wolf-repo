794f3ccb4c549bf66d8cd1607c4d538aea55a24d
Elizabeth Sanabria <eas2062@rit.edu>
2023-09-19 10:46:52 -0400

    commit one

---
+++ ReplacementWord/ReplacementWord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplacementWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string, preferably containing the word 'no'");
+            string enteredString = Console.ReadLine();
+            var newString = enteredString.Replace("no", "yes");
+            Console.WriteLine(newString);
+        }
+    }
+}
