ea2b670bf3143d94f0e03468dd43349247eb44ce
Elizabeth Sanabria <eas2062@rit.edu>
2023-09-20 19:03:19 -0400

    commit final comments added

---
+++ WordQuotes/WordQuotes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordQuotes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string."); //The string
+            string userString = Console.ReadLine();
+            string[] splitString = userString.Split(' '); //Splitting the string by spaces
+            string resultString = string.Empty; //Creating an empty result string
+            foreach (string word in splitString) //For each loop to add the spaces
+            {
+                resultString += "\"" + word + "\" "; //adding the ""
+            }
+            Console.WriteLine(resultString); //printing result
+
+                
+
+        }
+    }
+}
