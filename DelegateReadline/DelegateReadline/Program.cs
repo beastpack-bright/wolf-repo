ca8cee039bd334f5a37930389a95edf84e0eae35
Elizabeth Sanabria <eas2062@rit.edu>
2023-09-24 17:04:51 -0400

    Commit One, Possibly Final (Uses ReadLine)

---
+++ DelegateReadline/DelegateReadline/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateReadline
+{
+
+    delegate string ReadLineDelegate(); //Setting up the delegate function declaration
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadLineDelegate customRead = Console.ReadLine; //Setting the custom variable to readLine
+            Console.WriteLine("Type in a value."); //Getting user input
+
+            string userInput = customRead(); //taking that input 
+            Console.WriteLine($"You entered {userInput}!"); //string alongside delegate input
+
+        }
+    }
+}
---
+++ MathQuiz/MathQuiz/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MathQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order myOrder = new Order();
+            myOrder.itemName = "bread";
+            myOrder.unitCost = 1;
+            myOrder.unitCount = 5;
+
+            Console.WriteLine("order total = " + myOrder.Order2());
+            Console.WriteLine(myOrder.Order3());
+
+        }
+        struct Order
+        {
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+
+            public double Order2()
+            {
+                return unitCount * unitCost;
+            }
+
+            public string Order3()
+            {
+               Console.WriteLine("", unitCount, itemName, "items at $", unitCost, "each, total cost $", Order2());
+                string stringFinal = Console.ReadLine();
+                return stringFinal;
+
+            }
+        }
+
+
+    }
+}
