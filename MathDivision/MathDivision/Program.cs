b6d1077d3737cf6c4a7ce5e02d88371755ddb8f6
Elizabeth Sanabria <eas2062@rit.edu>
2023-09-28 18:40:01 -0400

    commit

--- MathDivision/MathDivision/Program.cs
+++ MathDivision/MathDivision/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace UT1_BugSquash
+namespace MathDivision
 {
     class Program
     {
         // Calculate x^y for y > 0 using a recursive function
+
+        delegate double myRoundDel(double value, int roundTo);
+
+        static double myRound(double value, int roundTo)
+        {
+            double multiplier = Math.Pow(10, roundTo);
+            double roundedValue = Math.Round(value * multiplier) / multiplier;
+            return roundedValue;
+        }
+
         static void Main(string[] args)
         {
+            myRoundDel MyRoundDel = myRound;
+            double roundedValue = MyRoundDel(4.1729, 2);
+            Console.WriteLine(roundedValue);
         }
     }
 }
