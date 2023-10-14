6847d5508dbc45162a5b93774377d51e50803189
Elizabeth Sanabria <eas2062@rit.edu>
2023-10-13 23:17:08 -0400

    peoplelib

--- Courses/Courses/Class1.cs
+++ Courses/Courses/Class1.cs
@@ -13,37 +13,23 @@ namespace Courses
             Schedule thisSchedule;
             Random rand = new Random();
 
-            // Generate courses IGME-200 through IGME-299
             for (int i = 200; i < 300; ++i)
             {
-                // Using constructor
                 thisCourse = new Course($"IGME-{i}", $"Description for IGME-{i}");
-
-                // Create a new Schedule object
                 thisSchedule = new Schedule();
 
                 for (int dow = 0; dow < 7; ++dow)
                 {
-                    // 50% chance of the class being on this day of the week
                     if (rand.Next(0, 2) == 1)
                     {
-                        // Add to the daysOfWeek list
                         thisSchedule.DaysOfWeek.Add((DayOfWeek)dow);
-
-                        // Select a random hour of the day
                         int nHour = rand.Next(0, 24);
-
-                        // Set start and end times with a minute duration
-                        // Select a fixed date to allow time calculations
                         thisSchedule.StartTime = new DateTime(1, 1, 1, nHour, 0, 0);
                         thisSchedule.EndTime = new DateTime(1, 1, 1, nHour, 50, 0);
                     }
                 }
 
-                // Set the schedule for this course
                 thisCourse.CourseSchedule = thisSchedule;
-
-                // Add this course to the SortedList
                 this[thisCourse.CourseCode] = thisCourse;
             }
         }
@@ -73,7 +59,7 @@ namespace Courses
                 }
                 catch
                 {
-                    
+                    // Handle exceptions for duplicate key, if needed
                 }
             }
         }
@@ -89,10 +75,10 @@ namespace Courses
 
     public class Course
     {
-        public string CourseCode { get; set; }
-        public string Description { get; set; }
-        public string TeacherEmail { get; set; }
-        public Schedule CourseSchedule { get; set; }
+        public string CourseCode;
+        public string Description;
+        public string TeacherEmail;
+        public Schedule CourseSchedule;
 
         public Course(string courseCode, string description)
         {
@@ -103,9 +89,9 @@ namespace Courses
 
     public class Schedule
     {
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public List<DayOfWeek> DaysOfWeek { get; set; }
+        public DateTime StartTime;
+        public DateTime EndTime;
+        public List<DayOfWeek> DaysOfWeek;
 
         public Schedule()
         {
