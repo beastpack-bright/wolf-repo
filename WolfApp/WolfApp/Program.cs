12d6288fb8c945ead2aaf4994cfb17b16507d731
Elizabeth Sanabria <eas2062@rit.edu>
2023-10-27 16:24:54 -0400

    aaah! a wolf is coding my code!
        _
           / \      _-'
         _/|  \-''- _ /
    __-' { |          \
        /             \
        /       "o.  |o }
        |            \ ;
                      ',
           \_         __\
             ''-_    \.//
               / '-____'
              /
            _'
          _-'

---
+++ WolfApp/WolfApp/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+
+namespace WolfClasses
+{
+
+    //I like wolves still.
+    public abstract class Wolf
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public abstract string Sound();
+
+        public void Eat()
+        {
+            Console.WriteLine("Wolf is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Wolf is sleeping.");
+        }
+    }
+
+    public interface IHunt
+    {
+        string Hunt();
+    }
+
+    public interface IHowl
+    {
+        string Howl();
+    }
+
+ 
+    public class GreyWolf : Wolf, IHunt, IHowl
+    {
+        public override string Sound()
+        {
+            return "Howl";
+        }
+
+        public string Hunt()
+        {
+            return "Grey Wolf is hunting.";
+        }
+
+        public string Howl()
+        {
+            return "Grey Wolf is howling.";
+        }
+    }
+
+    public class TimberWolf : Wolf, IHunt, IHowl
+    {
+        public override string Sound()
+        {
+            return "Howl";
+        }
+
+        public string Hunt()
+        {
+            return "Grrr. Bark bark!";
+        }
+
+        public string Howl()
+        {
+            return "AUUUUUUUUU";
+        }
+    }
+
+    class Program
+    {
+        static void MyMethod(object obj)
+        {
+            if (obj is Wolf wolf)
+            {
+                Console.WriteLine($"Wolf is making sounds!: {wolf.Sound()}");
+                Console.WriteLine($"Wolf is eating:: {wolf.name} is eating.");
+                Console.WriteLine($"Wolf is sleeping: {wolf.name} is sleeping.");
+            }
+            if (obj is IHunt hunting)
+            {
+                Console.WriteLine(hunting.Hunt());
+            }
+            if (obj is IHowl howling)
+            {
+                Console.WriteLine(howling.Howl());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GreyWolf greyWolf = new GreyWolf
+            {
+                name = "Grey Wolf -> Fluffy",
+                age = 5
+            };
+
+            TimberWolf timberWolf = new TimberWolf
+            {
+                name = "Timber Wolf -> Fluffy Two",
+                age = 7
+            };
+
+            MyMethod(greyWolf);   
+            Console.WriteLine();
+
+            MyMethod(timberWolf); 
+        }
+    }
+
+}
+
+
