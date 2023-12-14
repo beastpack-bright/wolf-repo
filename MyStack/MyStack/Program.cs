0f6b8cf8f74df6660cf6093747595acc7baa819f
Elizabeth Sanabria <eas2062@rit.edu>
2023-12-14 16:21:48 -0500

    mystack

---
+++ MyStack/MyStack/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    private List<int> stackList;
+
+    public MyStack()
+    {
+        stackList = new List<int>();
+    }
+    //pushing 
+    public void Push(int n)
+    {
+        stackList.Add(n);
+    }
+
+    //popping 
+    public int Pop()
+    {
+        if (stackList.Count == 0)
+        {//error handling
+            throw new InvalidOperationException("Stack is empty, cannot pop.");
+        }
+
+        int poppedItem = stackList[stackList.Count - 1];
+        stackList.RemoveAt(stackList.Count - 1);
+        return poppedItem;
+    }
+
+    public int Peek()
+    {//error handling
+        if (stackList.Count == 0)
+        {
+            throw new InvalidOperationException("Stack is empty, cannot peek.");
+        }
+
+        return stackList[stackList.Count - 1];
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //example usage using basic numbers (testing) 
+        MyStack myStack = new MyStack();
+       
+        myStack.Push(1);
+        myStack.Push(2);
+        myStack.Push(3);
+
+        Console.WriteLine("Peek: " + myStack.Peek()); //3 
+
+        Console.WriteLine("Pop: " + myStack.Pop());   // 3
+        Console.WriteLine("Pop: " + myStack.Pop());   // 2
+
+        myStack.Push(4);
+        Console.WriteLine("Peek: " + myStack.Peek()); // 4
+    }
+    //success! 
+}
