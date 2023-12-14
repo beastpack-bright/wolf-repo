4e9d3fe49be3e5d23245ba6d89646140fd165fbf
Elizabeth Sanabria <eas2062@rit.edu>
2023-12-14 18:02:56 -0500

    aaa

--- ColorsGraphDepth/ColorsGraphDepth/Program.cs
+++ ColorsGraphDepth/ColorsGraphDepth/Program.cs
@@ -22,13 +22,13 @@ namespace ColorsGraphDepth
         {
             // LightBlue, DarkBlue, Red, Grey, Orange, Purple, Yellow, Green
             { false, true, false, true, false, false, false, false }, // LightBlue
-            { true, false, false, false, false, false, false, false }, // DarkBlue
+            { true, false, true, false, false, false, false, false }, // DarkBlue
             { false, false, false, true, false, false, false, false }, // Red
             { true, false, false, false, true, false, false, false }, // Grey
             { false, false, false, true, false, true, false, false }, // Orange
             { false, false, false, false, false, false, true, false }, // Purple
             { false, false, false, false, false, true, false, false }, // Yellow
-            { false, false, false, false, false, false, true, false }  // Green
+            { false, false, false, false, false, false, false, false }  // Green
         };
 
         //list
@@ -36,13 +36,13 @@ namespace ColorsGraphDepth
         {
             // LightBlue, DarkBlue, Red, Grey, Orange, Purple, Yellow, Green
             new List<int> { 1, 3 }, // LightBlue
-            new List<int> { 0 },    // DarkBlue
-            new List<int> { 3 },    // Red
-            new List<int> { 0, 2, 4 }, // Grey
-            new List<int> { 3, 5 },    // Orange
-            new List<int> { 5 },    // Purple
-            new List<int> { 4, 6 },    // Yellow
-            new List<int> { 6 }     // Green
+            new List<int> { 0, 8 }, // DarkBlue
+            new List<int> { 1, 5 }, // Red
+            new List<int> { 0, 1, 4 }, // Grey
+            new List<int> { 3, 5 }, // Orange
+            new List<int> { 1 }, // Purple
+            new List<int> { 6 }, // Yellow
+            new List<int> { } // Green
         };
 
         //keep track of visited 
@@ -53,9 +53,8 @@ namespace ColorsGraphDepth
             Console.WriteLine("Depth-First Search starting from Red:");
 
             // start from red at index 2
-            DFS(2);
+            DFS((int)Colors.Red);
 
-           
 
             Console.ReadLine(); // pause view output
         }
@@ -65,9 +64,9 @@ namespace ColorsGraphDepth
             Console.WriteLine((Colors)vertex); // output current color
             visited[vertex] = true;
 
-            foreach (int neighbor in adjacencyList[vertex])
+            for (int neighbor = 0; neighbor < adjacencyMatrix.GetLength(1); neighbor++)
             {
-                if (!visited[neighbor])
+                if (adjacencyMatrix[vertex, neighbor] && !visited[neighbor])
                 {
                     DFS(neighbor);
                 }
